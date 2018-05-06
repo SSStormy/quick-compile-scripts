f31356232c4fc56c0b1d365317f6e6c831485072
Justas Dabrila <justasdabrila@gmail.com>
2018-05-06 19:05:42 +0000

    bunch of changes

--- qcs.sh
+++ qcs.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
+########## Initialize
 function log() {
-    echo "--> $1"
+    echo $1
 }
 
 function warn() {
-    >&2 log "$@"
+    >&2 log "--> $@"
 }
 
 if [ "$QCS_SAVE_FILE" = "" ] ; then
-    warn "QCS_SAVE_FILE not exported."
-    exit 1
+    warn "QCS_SAVE_FILE not exported, falling back to default."
+    QCS_SAVE_FILE=~/.qcs_save_file
 fi
 
+########## Language string setup
+# if you want to expand the scipt, start here.
 QCS_LANGS="cpp rust-bin rust-lib"
 QCS_LANGS_ARRAY=($QCS_LANGS)
 
@@ -20,6 +23,7 @@ QCS_LANG_CPP=${QCS_LANGS_ARRAY[0]}
 QCS_LANG_RUST_BIN=${QCS_LANGS_ARRAY[1]}
 QCS_LANG_RUST_LIB=${QCS_LANGS_ARRAY[2]}
 
+########## Deserialize QCS_SAVE_FILE
 data="$(cat $QCS_SAVE_FILE)"
 arr=($data)
 
@@ -27,88 +31,113 @@ QCS_PROJECT_DIR=${arr[0]}
 QCS_PROJECT_LANG=${arr[1]}
 QCS_TMUX_PANE=${arr[2]}
 
-QCS_OLD_DIR=$PWD
-
-if [ "$QCS_PROJECT_DIR" = "" ] ; then 
-    warn "Project dir is empty."
-    warn "Did you set up the project? (set)"
-    exit 1
-fi
-
-if [ "$QCS_PROJECT_LANG" = "" ] ; then 
-    warn "Lang is empty"
-    warn "Did you set up the project? (set)"
-    exit 1
-fi
-
-cd $QCS_PROJECT_DIR
-
-function send_to_output() {
+function send_key_to_output() {
     if [ "$QCS_TMUX_PANE" = "" ] ; then
         warn "No TMUX pane selected. (output)"
     else
-        tmux send-keys -t $QCS_TMUX_PANE "cd "$QCS_PROJECT_DIR"; clear; $@" C-m
+        tmux send-keys -t $@
     fi
 }
 
+function send_cmd_to_output() {
+    if [ "$QCS_TMUX_PANE" = "" ] ; then
+        warn "No TMUX pane selected. (output)"
+    else
+        tmux send-keys -t $QCS_TMUX_PANE " cd "$QCS_PROJECT_DIR"; clear; $@" C-m 
+    fi
+}
+
+function script_build() {
+    case $QCS_PROJECT_LANG in
+        $QCS_LANG_CPP) 
+            send_cmd_to_output "make -j8"
+            ;;
+        $QCS_LANG_RUST_BIN|$QCS_LANG_RUST_LIB)
+            send_cmd_to_output "cargo check"
+            ;;
+        *) 
+            send_cmd_to_output "echo Unknown language: $QCS_PROJECT_LANG"
+            ;;
+    esac
+}
+
+function script_run() {
+    case $QCS_PROJECT_LANG in 
+        $QCS_LANG_CPP) 
+            send_cmd_to_output "echo Running CPP projects is unsupported."
+            ;;
+        $QCS_LANG_RUST_LIB)
+            send_cmd_to_output "RUST_BACKTRACE=1 cargo build --lib"
+            ;;
+        $QCS_LANG_RUST_BIN)
+            send_cmd_to_output "RUST_BACKTRACE=1 cargo run"
+            ;;
+        *) 
+            send_cmd_to_output "echo Unknown language: $QCS_PROJECT_LANG"
+            ;;
+    esac
+}
+
+function script_test() {
+    case $QCS_PROJECT_LANG in
+        $QCS_LANG_CPP) 
+            send_to_putput "echo Testing CPP projects is unsupported."
+            ;;
+        $QCS_LANG_RUST_LIB|$QCS_LANG_RUST_BIN)
+            send_cmd_to_output "RUST_BACKTRACE=1 cargo test"
+            ;;
+        *) 
+            send_cmd_to_output "echo Unknown language: $QCS_PROJECT_LANG"
+            ;;
+    esac
+}
+
+function verify_save_stuff() {
+    if [ "$QCS_PROJECT_DIR" = "" ] ; then 
+        warn "Project dir is empty."
+        warn "Did you set up the project? (set)"
+        exit 1
+    fi
+
+    if [ "$QCS_PROJECT_LANG" = "" ] ; then 
+        warn "Lang is empty"
+        warn "Did you set up the project? (set)"
+        exit 1
+    fi
+}
+
+
+########## Handle args
 case $1 in
-    "build")
-        case $QCS_PROJECT_LANG in
-            $QCS_LANG_CPP) 
-                send_to_output "make -j8"
-                ;;
-            $QCS_LANG_RUST_BIN|$QCS_LANG_RUST_LIB)
-                send_to_output "cargo check"
-                ;;
-            *) 
-                send_to_output "echo Unknown language: $QCS_PROJECT_LANG"
-                ;;
-        esac
+    "build") 
+        verify_save_stuff
+        script_build
         ;;
-
     "run")
-        case $QCS_PROJECT_LANG in
-            $QCS_LANG_CPP) 
-                send_to_output "echo Running CPP projects is unsupported."
-                ;;
-            $QCS_LANG_RUST_LIB)
-                send_to_output "RUST_BACKTRACE=1 cargo build --lib"
-                ;;
-            $QCS_LANG_RUST_BIN)
-                send_to_output "RUST_BACKTRACE=1 cargo run"
-                ;;
-            *) 
-                send_to_output "echo Unknown language: $QCS_PROJECT_LANG"
-                ;;
-        esac
+        verify_save_stuff
+        script_run
         ;;
-
-    "test")
-        case $QCS_PROJECT_LANG in
-            $QCS_LANG_CPP) 
-                send_to_putput "echo Testing CPP projects is unsupported."
-                ;;
-            $QCS_LANG_RUST_LIB|$QCS_LANG_RUST_BIN)
-                send_to_output "RUST_BACKTRACE=1 cargo test"
-                ;;
-            *) 
-                send_to_output "echo Unknown language: $QCS_PROJECT_LANG"
-                ;;
-        esac
+    "test") 
+        verify_save_stuff
+        script_test
         ;;
-
     "set")
         if [ $2 = ""  ] ; then
             warn "--> Specify a language ($QCS_LANGS)."
         else
             for l in "${QCS_LANGS_ARRAY[@]}" ; do
-                if [ "$1" == "$l" ] ; then lang=$l ; fi
+                if [ "$2" == "$l" ] ; then lang=$l ; fi
             done
 
             if [ "$lang" == "" ] ; then
-                warn "Unknown language: $1"
+                warn "Unknown language: $2"
                 warn "Please choose one:"
                 warn "$QCS_LANGS"
+            else
+                QCS_PROJECT_LANG=$lang
+                QCS_PROJECT_DIR=$PWD
+                log "Set project dir to \"$QCS_PROJECT_DIR\""
+                log "Set project lang to \"$QCS_PROJECT_LANG\""
             fi
         fi
         ;;
@@ -125,14 +154,15 @@ case $1 in
     "dir")
         echo $QCS_PROJECT_DIR
         ;;
-
+    "terminate")
+        tmux send-keys -t $QCS_TMUX_PANE C-c
+        ;;
     *)
-        warn "Invalid arguments (build; run; test; set; output; jump)"
+        warn "Invalid arguments (build; run; test; set; output; jump; terminate)"
         ;;
 esac
 
-cd $QCS_OLD_DIR
-
+########## Serialize save file data
 echo $QCS_PROJECT_DIR > $QCS_SAVE_FILE
 echo $QCS_PROJECT_LANG >> $QCS_SAVE_FILE
 echo $QCS_TMUX_PANE >> $QCS_SAVE_FILE
